606acd0ae15a87b716ad4b81f26c4e2dc3a1fb36
Zachary Johnson <zachary.kaelan@gmail.com>
2024-09-30 23:33:39 -0400

    Make creature memory boxed

--- gridworld/src/grid/creature.rs
+++ gridworld/src/grid/creature.rs
@@ -1,14 +1,14 @@
 use crate::*;
 
 pub struct Creature {
-    pub memory: [f16; CREATURE_MEMORY],
+    pub memory: Box<[f16; CREATURE_MEMORY]>,
     pub energy: u16,
 }
 
 impl Default for Creature {
     fn default() -> Self {
         Self {
-            memory: [f16::from(0u8); CREATURE_MEMORY],
+            memory: Box::new([f16::from(0u8); CREATURE_MEMORY]),
             energy: consts::energy::CREATURE_START_ENERGY,
         }
     }
