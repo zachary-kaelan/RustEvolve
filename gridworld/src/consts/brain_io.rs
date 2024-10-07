d16802c6812660495f9306cb8410881e3ca0e774
zach.johnson <zach.johnson@Vaduminc.com>
2024-10-07 12:14:22 -0400

    Add OUTPUT_SIZE const

--- gridworld/src/consts/brain_io.rs
+++ gridworld/src/consts/brain_io.rs
@@ -3,3 +3,4 @@ use crate::consts::senses::EYE_CELLS_TOTAL;
 pub const NUM_TILES: usize = 4;
 pub const SLOTS_PER_EYE_CELL: usize = (NUM_TILES + 1) * 2;
 pub const INPUTS_SIZE: usize = (EYE_CELLS_TOTAL as usize) * (SLOTS_PER_EYE_CELL) + NUM_TILES;
+pub const OUTPUTS_SIZE: usize = 8;
